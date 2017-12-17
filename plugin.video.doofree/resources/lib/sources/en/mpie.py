91ced3d953f47e356bc82b21601bff8de92751f1
mpie <info@mpie.nl>
2017-12-17 16:19:00 +0100

    splitted

--- plugin.video.doofree/resources/lib/sources/en/mpie.py
+++ plugin.video.doofree/resources/lib/sources/en/mpie.py
@@ -90,7 +90,6 @@ class source:
             items = dom_parser2.parse_dom(contents, 'h3', attrs={'class': 'r'})
             items = [dom_parser2.parse_dom(i.content, 'a', req=['href']) for i in items]
             items = [(i[0].content, i[0].attrs['href']) for i in items]
-
             for item in items:
                 NAME = item[0]
                 movie_url = item[1]
@@ -117,8 +116,7 @@ class source:
                             if MOVIE[-4] == '.':
                                 if MOVIE.endswith('.mkv') or MOVIE.endswith('.mp4'):
                                     CLEANURL = URL.replace('%20', '.').lower()
-                                    if search_term.replace(' ', '.').replace('+', '.') in CLEANURL.replace(' ',
-                                                                                                           '.').lower():
+                                    if search_term.replace(' ', '.').replace('+', '.') in CLEANURL.replace(' ', '.').lower():
                                         if 'tvshowtitle' in data:
                                             SS = '%02d' % int(data['season'])
                                             EE = '%02d' % int(data['episode'])
@@ -135,8 +133,7 @@ class source:
 
                                                     if '.mkv' in MOVIE or '.mp4' in MOVIE:
                                                         sources.append(
-                                                            {'source': 'CDN', 'quality': qual, 'language': 'en',
-                                                             'url': MOVIE, 'direct': True, 'debridonly': False})
+                                                            {'source': 'CDN', 'quality': qual, 'language': 'en', 'url': MOVIE, 'direct': True, 'debridonly': False})
                                         else:
                                             if data['year'] in MOVIE.lower():
                                                 if '1080p' in MOVIE:
@@ -149,92 +146,7 @@ class source:
                                                     qual = 'SD'
 
                                                 if '.mkv' in MOVIE or '.mp4' in MOVIE:
-                                                    sources.append({'source': 'CDN', 'quality': qual, 'language': 'en',
-                                                                    'url': MOVIE, 'direct': True, 'debridonly': False})
-
-            # extra sources 1
-            extra_url = 'http://dl.hastidl.net/remotes/'
-
-            extra_contents = client.request(extra_url, headers=headers, timeout='3')
-
-            match = re.compile('<a href="(.+?)">(.+?)</a>').findall(extra_contents)
-
-            for url,name in match:
-                new_title = name.split('20')[0]
-                if cleantitle.get(title).lower() in cleantitle.get(new_title).lower():
-                    if 'tvshowtitle' in data:
-                        season = '%02d' % int(data['season'])
-                        episode = '%02d' % int(data['episode'])
-
-                        episode_chk = 's%se%s' % (season, episode)
-                        if episode_chk.lower() in url.lower():
-                            if '1080p' in url:
-                                qual = '1080p'
-                            elif '720p' in url:
-                                qual = '720p'
-                            elif '560p' in url:
-                                qual = 'SD'
-                            elif '480p' in url:
-                                qual = 'SD'
-                            else:
-                                qual = 'SD'
-                            url = extra_url + url
-                            sources.append({'source': 'CDN', 'quality': qual, 'language': 'en', 'url': url, 'direct': True, 'debridonly': False})
-                    else:
-                        if data['year'] in url:
-                            if '3D' in url:
-                                qual = '3D'
-                            elif '1080p' in url:
-                                qual = '1080p'
-                            elif '720p' in url:
-                                qual = '720p'
-                            elif '480p' in url:
-                                qual = 'SD'
-                            else:
-                                qual = 'SD'
-
-                            url = self.base_link + url
-                            sources.append({'source': 'CDN', 'quality': qual, 'language': 'en', 'url': url, 'direct': True, 'debridonly': False})
-
-            # extra sources 2
-            extra_url2 = 'http://dl3.melimedia.net/mersad/serial/'
-
-            extra_contents2 = client.request(extra_url2, headers=headers, timeout='3')
-
-            match = re.compile('<a href="(.+?)">(.+?)</a>').findall(extra_contents2)
-            for url, name in match:
-                if name[0] == ' ':
-                    name = name[1:]
-                name = name.replace('/', '')
-                url = extra_url2 + url
-                if title.lower().replace(' ', '') == name.lower().replace(' ', ''):
-                    html = client.request(url, timeout=5)
-                    match2 = re.compile('<a href="(.+?)">(.+?)</a>').findall(html)
-                    for url2, name2 in match2:
-                        season = '%02d' % int(data['season'])
-
-                        if 's' + season in url2:
-                            url2 = url + url2
-                            html2 = client.request(url2, timeout=5)
-                            match3 = re.compile('<a href="(.+?)">(.+?)</a>').findall(html2)
-                            for url3, name3 in match3:
-                                url3 = url2 + url3
-                                episode = '%02d' % int(data['episode'])
-                                if 'S' + season + 'E' + episode in url3:
-                                    if '1080p' in url3:
-                                        qual = '1080p'
-                                    elif '720p' in url3:
-                                        qual = '720p'
-                                    elif '560p' in url3:
-                                        qual = 'SD'
-                                    elif '480p' in url3:
-                                        qual = 'SD'
-                                    else:
-                                        qual = 'SD'
-
-                                    sources.append({'source': 'CDN', 'quality': qual, 'language': 'en', 'url': url3,
-                                                    'direct': True, 'debridonly': False})
-            print sources
+                                                    sources.append({'source': 'CDN', 'quality': qual, 'language': 'en', 'url': MOVIE, 'direct': True, 'debridonly': False})
             return sources
         except:
             return sources
